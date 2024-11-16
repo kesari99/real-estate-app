306242e6ebdab93bb6cb68e456e690b3c3adf3ce
Kesari Karna <kesaridevaraya@gmail.com>
2024-11-16 16:11:26 +0530

    created search api route

--- server/routes/listing-route.mjs
+++ server/routes/listing-route.mjs
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createListing, deleteUserListing, getListing, updateUserListing } from '../controllers/listingController.mjs'
+import { createListing, deleteUserListing, getListing, updateUserListing, getAllListings } from '../controllers/listingController.mjs'
 import { verifyToken } from '../utils/verifyUser.mjs'
 
 const router = express.Router()
@@ -8,6 +8,7 @@ router.post('/create',verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteUserListing)
 router.post('/update/:id', verifyToken, updateUserListing)
 router.get('/getlisting/:id', getListing)
+router.get('/get', getAllListings)
 
 
 
