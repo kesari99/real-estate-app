7aef2dde21176b18812d74bec1fffb79ced30b1c
Kesari Karna <kesaridevaraya@gmail.com>
2024-11-20 15:21:02 +0530

    chanfges

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ 
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173' || 'https://kesari-real-estate-app-frontend.onrender.com',
     credentials: true
 }));
 
@@ -41,11 +41,6 @@ app.use((err, req, res, next) => {
     })
 })
 
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-});
 
 
 
